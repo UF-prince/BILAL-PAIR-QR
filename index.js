5a68030e3ec5544824687f35da0c3829eb606612
UMAR • FAROOQ <ufprince1@gmail.com>
2025-06-20 23:35:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+
+let server = require('./qr');
+let code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/page.html')
-})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/qr', server);
+app.use('/code', code);
+
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'page.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`
-*_GIVE A STAR TO MY REPO BILAL-MD🌟_*
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
